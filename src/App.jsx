7ceee2aa12728b4406accc9e6eed8896adaf2f23
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-09-25 18:37:02 -0300

    :sparkles: docs: adicionando comentários

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,11 @@ function App() {
 
   const Weatherapi = async () => {
     const country = document.querySelector("#country");
-    
+
     //validando o campo do input, caso esteja vazio
     country.value.length == 0 ? alert("Digite o nome de uma cidade") : ''
 
+    //integração com a api do weather
     await axios.get(`http://api.weatherapi.com/v1/current.json?key=742ff23b82c349f499a214120232409&q=${country.value}&aqi=no`)
       .then((e) => {
         setLocation(e.data.location.name)
@@ -36,6 +37,7 @@ function App() {
 
         setTemperature(e.data.current.temp_c)
 
+        //traduzindo informações de EN para PT-BR do json
         if (e.data.current.condition.text == 'Partly cloudy') {
           setCondition('Parcialmente Nublado')
         } else if (e.data.current.condition.text == 'Clear') {
@@ -45,20 +47,20 @@ function App() {
         } else {
           setCondition(e.data.current.condition.text)
         }
-
+        //arredondando numeros quebrados e inserindo no useState
         setSensation(Math.round(e.data.current.feelslike_c) + 'º')
         setWind(Math.round(e.data.current.wind_kph) + 'KM/h')
         setHumidity(Math.round(e.data.current.humidity) + '%')
         setUV(e.data.current.uv)
 
-
-        console.log(e.data)
       })
 
+    //após o await rodar a animação da abertura do modal
     animationOpenScreen()
 
   }
 
+  //animação da abertura do modal
   const animationOpenScreen = () => {
     const screen = document.querySelector(".container__search__screen")
     screen.style.display = 'block'
@@ -70,6 +72,7 @@ function App() {
 
   }
 
+  //animação do fechamento do modal
   const closeAnimationOpenScreen = () => {
     const screen = document.querySelector(".container__search__screen")
     screen.style.opacity = 0
@@ -82,6 +85,8 @@ function App() {
     }, 200);
 
   }
+
+  //como a api não tem a minima e a maxima do dia, aqui adiciono 10% da temperatura do dia na maxima e retiro 10% na minima
   const MinTemperature = Math.round(temperature - temperature / 100 * 10) + 'º'
   const MaxTemperature = Math.round(temperature + temperature / 100 * 10) + 'º'
 
@@ -97,7 +102,6 @@ function App() {
             <strong>{location}, {region} - {country}</strong>
             <h1>{temperature}ºC<span>{condition}</span></h1>
             <div className="container__search__screen__average">
-
               <table className="container_search_screen_avarange_minmax">
                 <tbody>
                   <tr>
@@ -105,7 +109,6 @@ function App() {
                     <td className="container_search_screen_avarange_minmax__icon"><FontAwesomeIcon icon={faArrowUp} /> <strong>{MaxTemperature}</strong></td>
                     <td>Sensação <strong>{sensation}</strong></td>
                   </tr>
-
                   <tr>
                     <td>Vento <strong>{wind}</strong></td>
                     <td>Humidade <strong>{humidity}</strong></td>
